0066bd2aa0351387335afe6334e08311f25b2035
Scott J Maddox <scottjmaddox@gmail.com>
2024-11-24 13:42:13 -0600

    softcap attention scores

--- train_gpt2.py
+++ train_gpt2.py
@@ -188,7 +188,8 @@ class CausalSelfAttention(nn.Module):
         cos, sin = self.rotary(q)
         q, k = F.rms_norm(q, (q.size(-1),)), F.rms_norm(k, (k.size(-1),)) # QK norm suggested by @Grad62304977
         q, k = apply_rotary_emb(q, cos, sin), apply_rotary_emb(k, cos, sin)
-        y = flex_attention(q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2), block_mask=block_mask)
+        score_mod = lambda score, b, h, q_idx, kv_idx: 20 * torch.tanh(score / 20)
+        y = flex_attention(q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2), score_mod=score_mod, block_mask=block_mask)
         y = y.transpose(1, 2).contiguous().view_as(x) # re-assemble all head outputs side by side
         y = self.c_proj(y)
         return y, v1
